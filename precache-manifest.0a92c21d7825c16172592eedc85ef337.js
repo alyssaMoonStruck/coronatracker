7e4582abcfb29f5e8799fb8099467e5d416d7ef1
Alyssa DeMay <camocat11@gmail.com>
2020-12-12 16:59:48 -0600

    Updates

---
+++ precache-manifest.0a92c21d7825c16172592eedc85ef337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb74ac441b78341a2bd34e62dc000d24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15ed11a1b8a4991c57bd",
+    "url": "./static/css/main.0f80b0ee.chunk.css"
+  },
+  {
+    "revision": "dcc57c4a0751e7082ee2",
+    "url": "./static/js/2.bd71af1f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "./static/js/2.bd71af1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ed11a1b8a4991c57bd",
+    "url": "./static/js/main.805ecebf.chunk.js"
+  },
+  {
+    "revision": "8e6c43279035e2e0e7fd",
+    "url": "./static/js/runtime-main.35e29c5d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "./static/media/image.d7265326.png"
+  }
+]);
